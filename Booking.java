bc0b85079d853812143d90eb353f1f32459453e7
Ashfaaq-cmd <ashfaaqdulloo27@gmail.com>
2026-03-31 13:43:32 +0400

    First Commit

---
+++ Booking.java
@@ -0,0 +1,69 @@
+
+public class Booking {
+private int id;
+private int userId;
+private int sessionId;
+private String bookingDate;
+private String status;
+private String sessionTitle;
+private String sessionDate;
+private String sessionTime;
+private double price;
+
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public int getUserId() {
+	return userId;
+}
+public void setUserId(int userId) {
+	this.userId = userId;
+}
+public int getSessionId() {
+	return sessionId;
+}
+public void setSessionId(int sessionId) {
+	this.sessionId = sessionId;
+}
+public String getBookingDate() {
+	return bookingDate;
+}
+public void setBookingDate(String bookingDate) {
+	this.bookingDate = bookingDate;
+}
+public String getStatus() {
+	return status;
+}
+public void setStatus(String status) {
+	this.status = status;
+}
+public String getSessionTitle() {
+	return sessionTitle;
+}
+public void setSessionTitle(String sessionTitle) {
+	this.sessionTitle = sessionTitle;
+}
+public String getSessionDate() {
+	return sessionDate;
+}
+public void setSessionDate(String sessionDate) {
+	this.sessionDate = sessionDate;
+}
+public String getSessionTime() {
+	return sessionTime;
+}
+public void setSessionTime(String sessionTime) {
+	this.sessionTime = sessionTime;
+}
+public double getPrice() {
+	return price;
+}
+public void setPrice(double price) {
+	this.price = price;
+}
+
+
+}
---
+++ Payment.java
@@ -0,0 +1,73 @@
+
+public class Payment {
+	private int id;
+	private int bookingId;
+	private double amount;
+	private String paymentDate;
+	private String paymentStatus;
+	//For Display
+	private String studentName;
+	private String sessionTitle;
+	
+	public Payment() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getBookingId() {
+		return bookingId;
+	}
+
+	public void setBookingId(int bookingId) {
+		this.bookingId = bookingId;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public String getPaymentDate() {
+		return paymentDate;
+	}
+
+	public void setPaymentDate(String paymentDate) {
+		this.paymentDate = paymentDate;
+	}
+
+	public String getPaymentStatus() {
+		return paymentStatus;
+	}
+
+	public void setPaymentStatus(String paymentStatus) {
+		this.paymentStatus = paymentStatus;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public String getSessionTitle() {
+		return sessionTitle;
+	}
+
+	public void setSessionTitle(String sessionTitle) {
+		this.sessionTitle = sessionTitle;
+	}
+	
+}
